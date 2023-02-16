6e323901dd25cf5b90438e18762a43392ba7aee0
sonyewonnn <cristal789@naver.com>
2023-02-16 10:40:16 +0900

    figma

---
+++ figma.js
@@ -0,0 +1,9 @@
+function addNewBlock (){
+    let boxWrapper = document.getElementById("box-wrapper")
+    boxWrapper.insertAdjacentHTML('beforeend', `
+        <div id="second"></div>
+    `)
+}
+
+
+
